53f485cad9a72640b7f204634267f40ab56147b9
Harsh-Veliyat <83662106+Harsh-Veliyat@users.noreply.github.com>
2021-08-28 08:40:54 +0530

    Update MiniProject.cpp

--- MiniProject/MiniProject.cpp
+++ MiniProject/MiniProject.cpp
@@ -1,344 +1,426 @@
-#include <iostream>
-#include<vector>
-#include<queue>
-#include<map>
-#include<string>
+	#include <iostream>
+
 using namespace std;
 
-struct ps {
-	string to;
-	int rs;
+enum Color{black,red};
+
+struct RBT_Node {
+	int val; 
+	RBT_Node* parent; 
+	RBT_Node* lchild; 
+	RBT_Node* rchild; 
+	Color color; 
 };
 
-typedef map<string, vector<ps>>::iterator mIt;
-typedef vector<ps>::iterator vIt;
+typedef RBT_Node* rbtPtr;
 
-class Graph {
+class RBTree {
 private:
-	ps nullPsVar = { "NULL",0 };
-	map<string, vector<ps>> edge;
-	
-	// Inserts new edge in Graph
-	void insertEdge(string from, string to, int val)
-	{
-		ps temp = { to, val };
-		edge[from].push_back(temp);
-	}
+	rbtPtr root;
+	rbtPtr NullNode;
 
-	// Deletes an specified edge from graph
-	void deleteEdge(string from, string to) {
-		vIt i;
-		for (i = edge[from].begin(); i != edge[from].end(); i++)
-		{
-			if (i->to == to)
-				break;
-		}
-		edge[from].erase(i);
-	}
-
-	// Updates value of specified edge
-	void updateEdge(string from, string to, int val) {
-		for (vIt i = edge[from].begin(); i != edge[from].end(); i++)
-		{
-			if (i->to == to)
-			{
-				i->rs = val;
-				return;
-			}
+	void printPreOrder(rbtPtr node) {
+		if (node != NullNode) {
+			cout << node->val << " ";
+			printPreOrder(node->lchild);
+			printPreOrder(node->rchild);
 		}
 	}
 
-	// Checks if the object with specified name exists as key or not
-	vector<ps> CheckIfIndexExists(string s)
-	{
-		for (mIt i = edge.begin(); i != edge.end(); i++)
-		{
-			if (i->first == s)
-				return i->second;
+	void printInOrder(rbtPtr node) {
+		if (node != NullNode) {
+			printInOrder(node->lchild);
+			cout << node->val << " ";
+			printInOrder(node->rchild);
 		}
-		return {};
 	}
-	
-	vector<ps> checkIFxExists(string s) {
-		return CheckIfIndexExists(s);
-	}
-	
-	//Checks wether the specified name exists in the vector of struct we entered as value corresponding to the key.
-	ps checkStringInVector(string s, vector<ps> v) {
-		for (vIt i = v.begin(); i != v.end(); i++)
-		{
-			if (i->to == s)
-				return *i;
+
+	void printPostOrder(rbtPtr node) {
+		if (node != NullNode) {
+			printPostOrder(node->lchild);
+			printPostOrder(node->rchild);
+			cout << node->val << " ";
 		}
-		ps temp = { "NULL",0 };
-		return temp;
 	}
-	
-	//Returns total rs that the specified person has to pay
-	int totalDebtOfX(string s)
-	{
-		vector<ps> checkDebtor = checkIFxExists(s);
-		if (checkDebtor.empty())
-			return 0;
-		else
-		{
-			int sum{};
-			for (vIt j = edge[s].begin(); j != edge[s].end(); j++)
-			{
-				sum += j->rs;
-			}
-			for (mIt i = edge.begin(); i != edge.end(); i++)
-			{
-				if (sum < 0)
-					return 0;
-				if (i->first == s)
-					continue;
-				else
-				{
-					ps checkDebtorInVector = checkStringInVector(s, edge[i->first]);
-					if (checkDebtorInVector.to != nullPsVar.to)
-					{
-						sum -= checkDebtorInVector.rs;
+
+	rbtPtr findVal(rbtPtr node, int key) {
+		if (node == NullNode || key == node->val) {
+			return node;
+		}
+
+		if (key < node->val) {
+			return findVal(node->lchild, key);
+		}
+		return findVal(node->rchild, key);
+	}
+
+	void fixDelete(rbtPtr x) {
+		rbtPtr s;
+		while (x != root && x->color == black) {
+			if (x == x->parent->lchild) {
+				s = x->parent->rchild;
+				if (s->color == red) {
+					// case 3.1
+					s->color = black;
+					x->parent->color = red;
+					leftRotate(x->parent);
+					s = x->parent->rchild;
+				}
+
+				if (s->lchild->color == black && s->rchild->color == black) {
+					// case 3.2
+					s->color = red;
+					x = x->parent;
+				}
+				else {
+					if (s->rchild->color == black) {
+						// case 3.3
+						s->lchild->color = black;
+						s->color = red;
+						rightRotate(s);
+						s = x->parent->rchild;
 					}
+
+					// case 3.4
+					s->color = x->parent->color;
+					x->parent->color = black;
+					s->rchild->color = black;
+					leftRotate(x->parent);
+					x = root;
 				}
 			}
-			return sum;
+			else {
+				s = x->parent->lchild;
+				if (s->color == red) {
+					// case 3.1
+					s->color = black;
+					x->parent->color = red;
+					rightRotate(x->parent);
+					s = x->parent->lchild;
+				}
+
+				if (s->lchild->color == black && s->rchild->color == black) {
+					// case 3.2
+					s->color = red;
+					x = x->parent;
+				}
+				else {
+					if (s->lchild->color == black) {
+						// case 3.3
+						s->rchild->color = black;
+						s->color = red;
+						leftRotate(s);
+						s = x->parent->lchild;
+					}
+
+					// case 3.4
+					s->color = x->parent->color;
+					x->parent->color = black;
+					s->lchild->color = black;
+					rightRotate(x->parent);
+					x = root;
+				}
+			}
 		}
+		x->color = black;
+	}
+
+
+	void rbTransplant(rbtPtr u, rbtPtr v) {
+		if (u->parent == nullptr) {
+			root = v;
+		}
+		else if (u == u->parent->lchild) {
+			u->parent->lchild = v;
+		}
+		else {
+			u->parent->rchild = v;
+		}
+		v->parent = u->parent;
+	}
+
+	void deleteNode(rbtPtr node, int key) {
+		// find the node containing key
+		rbtPtr z = NullNode;
+		rbtPtr x, y;
+		while (node != NullNode) {
+			if (node->val == key) {
+				z = node;
+			}
+
+			if (node->val <= key) {
+				node = node->rchild;
+			}
+			else {
+				node = node->lchild;
+			}
+		}
+
+		if (z == NullNode) {
+			cout << "Couldn't find key in the tree" << endl;
+			return;
+		}
+
+		y = z;
+		int y_original_color = y->color;
+		if (z->lchild == NullNode) {
+			x = z->rchild;
+			rbTransplant(z, z->rchild);
+		}
+		else if (z->rchild == NullNode) {
+			x = z->lchild;
+			rbTransplant(z, z->lchild);
+		}
+		else {
+			y = minimum(z->rchild);
+			y_original_color = y->color;
+			x = y->rchild;
+			if (y->parent == z) {
+				x->parent = y;
+			}
+			else {
+				rbTransplant(y, y->rchild);
+				y->rchild = z->rchild;
+				y->rchild->parent = y;
+			}
+
+			rbTransplant(z, y);
+			y->lchild = z->lchild;
+			y->lchild->parent = y;
+			y->color = z->color;
+		}
+		delete z;
+		if (y_original_color == black) {
+			fixDelete(x);
+		}
+	}
+
+	void fixInsert(rbtPtr k) {
+		rbtPtr u;
+		while (k->parent->color == red) {
+			if (k->parent == k->parent->parent->rchild) {
+				u = k->parent->parent->lchild; // uncle
+				if (u->color == red) {
+					// case 3.1
+					u->color = black;
+					k->parent->color = black;
+					k->parent->parent->color = red;
+					k = k->parent->parent;
+				}
+				else {
+					if (k == k->parent->lchild) {
+						// case 3.2.2
+						k = k->parent;
+						rightRotate(k);
+					}
+					// case 3.2.1
+					k->parent->color = black;
+					k->parent->parent->color = red;
+					leftRotate(k->parent->parent);
+				}
+			}
+			else {
+				u = k->parent->parent->rchild; // uncle
+
+				if (u->color == red) {
+					// mirror case 3.red
+					u->color = black;
+					k->parent->color = black;
+					k->parent->parent->color = red;
+					k = k->parent->parent;
+				}
+				else {
+					if (k == k->parent->rchild) {
+						// mirror case 3.2.2
+						k = k->parent;
+						leftRotate(k);
+					}
+					// mirror case 3.2.1
+					k->parent->color = black;
+					k->parent->parent->color = red;
+					rightRotate(k->parent->parent);
+				}
+			}
+			if (k == root) {
+				break;
+			}
+		}
+		root->color = black;
+	}
+
+	void printT(rbtPtr root, string indent, bool last) {
+		// print the tree structure on the screen
+		if (root != NullNode) {
+			cout << indent;
+			if (last) {
+				cout << "R---> ";
+				indent += "     ";
+			}
+			else {
+				cout << "L---> ";
+				indent += "|    ";
+			}
+
+			string sColor = root->color ? "RED" : "BLACK";
+			cout << root->val << "(" << sColor << ")" << endl;
+			printT(root->lchild, indent, false);
+			printT(root->rchild, indent, true);
+		}
+		// cout<<root->lchild->val<<endl;
 	}
 
 public:
-	void addNewEntry(string from, string to, int val) {
-		
-		if (from == to)
-		{
-			cout << "Invalid Entry!" << endl;
+	RBTree() {
+		NullNode = new RBT_Node;
+		NullNode->color = black;
+		NullNode->lchild = nullptr;
+		NullNode->rchild = nullptr;
+		root = NullNode;
+	}
+
+	void preorder() {
+		printPreOrder(this->root);
+	}
+
+	void inorder() {
+		printInOrder(this->root);
+	}
+
+	void postorder() {
+		printPostOrder(this->root);
+	}
+
+	rbtPtr searchTree(int k) {
+		return findVal(this->root, k);
+	}
+
+	rbtPtr minimum(rbtPtr node) {
+		while (node->lchild != NullNode) {
+			node = node->lchild;
+		}
+		return node;
+	}
+
+	void leftRotate(rbtPtr x) {
+		rbtPtr y = x->rchild;
+		x->rchild = y->lchild;
+		if (y->lchild != NullNode) {
+			y->lchild->parent = x;
+		}
+		y->parent = x->parent;
+		if (x->parent == nullptr) {
+			this->root = y;
+		}
+		else if (x == x->parent->lchild) {
+			x->parent->lchild = y;
+		}
+		else {
+			x->parent->rchild = y;
+		}
+		y->lchild = x;
+		x->parent = y;
+	}
+
+	void rightRotate(rbtPtr x) {
+		rbtPtr y = x->lchild;
+		x->lchild = y->rchild;
+		if (y->rchild != NullNode) {
+			y->rchild->parent = x;
+		}
+		y->parent = x->parent;
+		if (x->parent == nullptr) {
+			this->root = y;
+		}
+		else if (x == x->parent->rchild) {
+			x->parent->rchild = y;
+		}
+		else {
+			x->parent->lchild = y;
+		}
+		y->rchild = x;
+		x->parent = y;
+	}
+
+	void insert(int key) {
+
+		rbtPtr node = new RBT_Node;
+		node->parent = nullptr;
+		node->val = key;
+		node->lchild = NullNode;
+		node->rchild = NullNode;
+		node->color = red; 
+
+		rbtPtr y = nullptr;
+		rbtPtr x = this->root;
+
+		while (x != NullNode) {
+			y = x;
+			if (node->val < x->val) {
+				x = x->lchild;
+			}
+			else {
+				x = x->rchild;
+			}
+		}
+
+		node->parent = y;
+		if (y == nullptr) {
+			root = node;
+		}
+		else if (node->val < y->val) {
+			y->lchild = node;
+		}
+		else {
+			y->rchild = node;
+		}
+
+		if (node->parent == nullptr) {
+			node->color = black;
 			return;
 		}
-		vector<ps> checkTo = checkIFxExists(to);
-		if (!checkTo.empty())
-		{
-			ps checkFromInVector = checkStringInVector(from, checkTo);
-			if (checkFromInVector.to != nullPsVar.to)
-			{
-				if (val <= checkFromInVector.rs)
-				{
-					if (val == checkFromInVector.rs)
-					{
-						deleteEdge(to, from);
-						return;
-					}
-					else
-						updateEdge(to, from, checkFromInVector.rs - val);
-					return;
-				}
-				else
-				{
-					val = val - checkFromInVector.rs;
-					deleteEdge(to, from);
-					insertEdge(from, to, val);
-					return;
-				}
-			}
+
+		if (node->parent->parent == nullptr) {
+			return;
 		}
-		vector<ps> checkFrom = checkIFxExists(from);
-		if (!checkFrom.empty())
-		{
-			ps checkToInVector = checkStringInVector(to, checkFrom);
-			if (checkToInVector.to != nullPsVar.to)
-			{
-				updateEdge(from, to, val + checkToInVector.rs);
-				return;
-			}
-		}
-		insertEdge(from, to, val);
-		return;
+		fixInsert(node);
 	}
-	void print()
-	{
-		for (mIt i = edge.begin(); i != edge.end(); i++)
-		{
-			for (vIt j = i->second.begin(); j != i->second.end(); j++)
-			{
-				cout << i->first << " owes " << j->rs << "rs to " << j->to << endl;
-			}
+
+	rbtPtr getRoot() {
+		return this->root;
+	}
+
+	void deleteVal(int val) {
+		deleteNode(this->root, val);
+	}
+
+	void printTree() {
+		if (root) {
+			printT(this->root, "", true);
 		}
 	}
-	void getTotalDebtofX(string s)
-	{
-		cout << s << " has to pay " << totalDebtOfX(s) << endl;
-	}
+
 };
 
-int main()
-{
-	int choice{};
-	Graph gph;
-	while (choice != 4)
-	{
-		system("CLS");
-		cout << "DISCLAIMER: ENTER ONLY FIRST NAME OF PERSON AND USE FIRST CHARACTER AS CAPITAL AND REST AS LOWERCASE LETTERS\n\nFor adding new Entry, enter 1.\nTo view the Graph, enter 2.\nTo know the debt of any person, enter 3.\nTo exit, enter 4;\n\nEnter your choice:";
-		cin >> choice;
-		switch (choice)
-		{
-			case 1:
-			{	
-				int val;
-				string from, to;
-				cout << "Enter the name of the Debtor:";
-				cin >> from;
-				cout << "Enter the name of the Creditor:";
-				cin >> to;
-				cout << "Enter amount:";
-				cin >> val;
-				gph.addNewEntry(from, to, val);
-				break;
-			}
-			case 2:
-			{
-				gph.print();
-				cin.ignore();
-				cin.ignore();
-				break;
-			}
-			case 3:
-			{
-				string name;
-				cout << "Enter the name of person whose total debt you want:";
-				cin >> name;
-				gph.getTotalDebtofX(name);
-				cin.ignore();
-				cin.ignore();
-				break;
-			}
-			case 4:
-			{
-				exit(0);
-			}
-			default:
-			{
-				cout << "Invalid Entry!\nRestart the program and enter correct option." << endl;
-				exit(0);
-			}
-		}
-	}
-	
-	//gph.addNewEntry("C", "A", 20);
-	//gph.addNewEntry("A", "B", 30);
-	//gph.addNewEntry("D", "F", 10);
-	//gph.addNewEntry("D", "A", 50);
-	//gph.print();// c -> a, a-b, d-f-a
-	//cout << endl;
-	//gph.addNewEntry("B", "A", 50);
-	//gph.print();// c-a, b-a-20, d-f-a
-	//cout << endl;
-	//gph.addNewEntry("C", "A", 10);
-	//gph.print();//c-a-30, b-a-20, d-f-a
-	//cout << endl;
-	//gph.addNewEntry("A", "C", 50);
-	//gph.print();//a-c-20, b-a-20, d-f-a
-	//cout << endl;
-	//gph.addNewEntry("A", "A", 100);
-	//gph.print();// invalid ''
+int main() {
+	RBTree rbtree;
+	rbtree.insert(18);
+	rbtree.insert(6);
+	rbtree.insert(20);
+	rbtree.insert(3);
+	rbtree.insert(4);
+	rbtree.insert(25);
+	rbtree.insert(11);
+	rbtree.insert(10);
+	rbtree.insert(5);
+	rbtree.insert(1);
+	rbtree.printTree();
+	cout << endl;
+	rbtree.deleteVal(17);
+	cout << endl;
+	rbtree.printTree();
+	cout << endl;
+	rbtree.deleteVal(10);
+	cout << endl;
+	rbtree.printTree();
+	return 0;
 }
-
-
-
-/*
-* // First "To", "From"
-* 
-* map<string,vector<struct(string, int)>>
-* B -> ([A]->50), ([C]->60)
-* [B] -> [A]->70
-* A - 20 -> B
-* 
-* 
-* B -> (A,30)
-* 
-* A -20 -> B
-
- B -> (A,10) 
-
- A - 30 -> B
-
- after check
-   A - 20 -> B
-A -> (B,20)
-
-
-A has to pay B 30		
-* A has to B 40* 
-* A - 70 -> B			
-* 180 > 100
-* 80
-* B has to pay 80 // B - 180 -> A
-* B - 10 -> A	
-	AddNew-> int Check(bool, int)-> Insert
-	Check
-	Insert (A, B, val)
-
-  B -> (A,50), (C,10)
-*
-* A - 20 -> B
-
-	Methods:
-	vector<> CheckIfIndexExists(string s)
-	{
-		
-	}
-	vector<> checkIFFromExists(string from){
-		return CheckIfIndexExists(from);
-	}
-	vector<> checkIfToExists(string to){
-		return CheckIfIndexExists(to);
-	}
-	
-	pair<string,int> checkStringInVector(string s , vector v){}
-
-	void insert(string from, string to, int val)
-	{
-		
-	}
-
-	void delete(string from, string to){}
-
-	void update(string from, string too, int val){}
-
-  B -> (A,50), (C,10)
-
-* A - 20 -> B
-* A - from
-* B - to
-* x = 20
-* vector checkTo = checkIfToExists("B");
-* if(checkTo != NULL)
-* {
-*	checkFromInVector = checkStringInVector(A, checkTo)
-*	if(checkFromInVector != NULL)
-*	{
-*		if(x <= val)
-*			update(to, from, val - x) // add if update delete
-*			return;
-*		else
-*		{
-*			x = x - val;
-*			delete(to, from)
-*			insert(from, to, x)
-*			return;
-*		}
-*	}
-* }
-*
-*	checkFrom = checkIfFromExists(A);
-*	if(checkFrom != NULL)
-*	{
-*		checkToInVector = checkStringInVector(B, checkFrom)
-*		if(checkToInVector != NULL)
-*		{
-*			update(from, to, x + val);
-*			return;
-*		}
-*	
-*	
-*insert(from, to, x);
-* return;
-*/ 
